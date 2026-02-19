9f8e32fb0605018bffb8894155788f52bdc9d0c7
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-02-19 10:50:54 +0000

    test: add CESR test vectors from WebOfTrust/cesr-test-vectors
    Encode/decode vectors for codes D, E, 0B with known raw bytes
    matching the official cesr-test-vectors repository.

--- test/spec/Keri/Cesr/DecodeSpec.hs
+++ test/spec/Keri/Cesr/DecodeSpec.hs
@@ -80,6 +80,141 @@ spec = do
         it "rejects unknown codes" $
             decode "Zxxxxxxxxx"
                 `shouldSatisfy` isLeft
+
+    describe "cesr-test-vectors" $ do
+        -- Vectors from WebOfTrust/cesr-test-vectors
+        let raw32 =
+                BS.pack
+                    [ 0x5f
+                    , 0x9c
+                    , 0xd8
+                    , 0x45
+                    , 0xa8
+                    , 0x9d
+                    , 0xab
+                    , 0x7b
+                    , 0xf0
+                    , 0xde
+                    , 0xe3
+                    , 0x3d
+                    , 0xa6
+                    , 0xf0
+                    , 0xa5
+                    , 0x96
+                    , 0xe1
+                    , 0x89
+                    , 0x05
+                    , 0xcf
+                    , 0xc2
+                    , 0x85
+                    , 0x29
+                    , 0x15
+                    , 0x5f
+                    , 0xa8
+                    , 0xd2
+                    , 0x94
+                    , 0x31
+                    , 0x9e
+                    , 0xdf
+                    , 0xe0
+                    ]
+
+        it "decodes code D" $ do
+            let qb64 =
+                    "DF-c2EWonat78N7jPabwpZbhiQXPwoUpFV-o0pQxnt_g"
+            case decode qb64 of
+                Left err -> expectationFailure err
+                Right prim -> do
+                    code prim
+                        `shouldBe` Ed25519PubKey
+                    raw prim `shouldBe` raw32
+
+        it "decodes code E" $ do
+            let qb64 =
+                    "EF-c2EWonat78N7jPabwpZbhiQXPwoUpFV-o0pQxnt_g"
+            case decode qb64 of
+                Left err -> expectationFailure err
+                Right prim -> do
+                    code prim
+                        `shouldBe` Blake2bDigest
+                    raw prim `shouldBe` raw32
+
+        it "decodes code 0B" $ do
+            let qb64 =
+                    "0BA-meOtrZqh25qrCF30C_TuNDz5Et2F3t2rLbJ98TqXCelY8I4ZpkZQy7iD93SRxZTATQTFQF99U0XNA_JPgq0N"
+                raw64 =
+                    BS.pack
+                        [ 0x3e
+                        , 0x99
+                        , 0xe3
+                        , 0xad
+                        , 0xad
+                        , 0x9a
+                        , 0xa1
+                        , 0xdb
+                        , 0x9a
+                        , 0xab
+                        , 0x08
+                        , 0x5d
+                        , 0xf4
+                        , 0x0b
+                        , 0xf4
+                        , 0xee
+                        , 0x34
+                        , 0x3c
+                        , 0xf9
+                        , 0x12
+                        , 0xdd
+                        , 0x85
+                        , 0xde
+                        , 0xdd
+                        , 0xab
+                        , 0x2d
+                        , 0xb2
+                        , 0x7d
+                        , 0xf1
+                        , 0x3a
+                        , 0x97
+                        , 0x09
+                        , 0xe9
+                        , 0x58
+                        , 0xf0
+                        , 0x8e
+                        , 0x19
+                        , 0xa6
+                        , 0x46
+                        , 0x50
+                        , 0xcb
+                        , 0xb8
+                        , 0x83
+                        , 0xf7
+                        , 0x74
+                        , 0x91
+                        , 0xc5
+                        , 0x94
+                        , 0xc0
+                        , 0x4d
+                        , 0x04
+                        , 0xc5
+                        , 0x40
+                        , 0x5f
+                        , 0x7d
+                        , 0x53
+                        , 0x45
+                        , 0xcd
+                        , 0x03
+                        , 0xf2
+                        , 0x4f
+                        , 0x82
+                        , 0xad
+                        , 0x0d
+                        ]
+            case decode qb64 of
+                Left err -> expectationFailure err
+                Right prim -> do
+                    code prim
+                        `shouldBe` Ed25519Sig
+                    raw prim `shouldBe` raw64
   where
     isLeft (Left _) = True
     isLeft _ = False
--- test/spec/Keri/Cesr/EncodeSpec.hs
+++ test/spec/Keri/Cesr/EncodeSpec.hs
@@ -82,8 +82,126 @@ spec = do
                             ]
                     prim =
                         Primitive Ed25519PubKey verkey
-                    -- keripy Verfer with code=Ed25519
-                    -- gives qb64 with prefix 'D'
                     expected =
                         "DKxy2sgzfplyr-tgwIxS19f2OchFHtLwPWD3v4oYimBx"
                 encode prim `shouldBe` expected
+
+    describe "cesr-test-vectors" $ do
+        -- Vectors from WebOfTrust/cesr-test-vectors
+        -- Same 32-byte raw value encoded with D/E
+        let raw32 :: ByteString
+            raw32 =
+                BS.pack
+                    [ 0x5f
+                    , 0x9c
+                    , 0xd8
+                    , 0x45
+                    , 0xa8
+                    , 0x9d
+                    , 0xab
+                    , 0x7b
+                    , 0xf0
+                    , 0xde
+                    , 0xe3
+                    , 0x3d
+                    , 0xa6
+                    , 0xf0
+                    , 0xa5
+                    , 0x96
+                    , 0xe1
+                    , 0x89
+                    , 0x05
+                    , 0xcf
+                    , 0xc2
+                    , 0x85
+                    , 0x29
+                    , 0x15
+                    , 0x5f
+                    , 0xa8
+                    , 0xd2
+                    , 0x94
+                    , 0x31
+                    , 0x9e
+                    , 0xdf
+                    , 0xe0
+                    ]
+
+        it "code D (Ed25519 transferable)" $
+            encode (Primitive Ed25519PubKey raw32)
+                `shouldBe` "DF-c2EWonat78N7jPabwpZbhiQXPwoUpFV-o0pQxnt_g"
+
+        it "code E (Blake3/2b digest)" $
+            encode (Primitive Blake2bDigest raw32)
+                `shouldBe` "EF-c2EWonat78N7jPabwpZbhiQXPwoUpFV-o0pQxnt_g"
+
+        it "code 0B (Ed25519 signature)" $ do
+            let raw64 :: ByteString
+                raw64 =
+                    BS.pack
+                        [ 0x3e
+                        , 0x99
+                        , 0xe3
+                        , 0xad
+                        , 0xad
+                        , 0x9a
+                        , 0xa1
+                        , 0xdb
+                        , 0x9a
+                        , 0xab
+                        , 0x08
+                        , 0x5d
+                        , 0xf4
+                        , 0x0b
+                        , 0xf4
+                        , 0xee
+                        , 0x34
+                        , 0x3c
+                        , 0xf9
+                        , 0x12
+                        , 0xdd
+                        , 0x85
+                        , 0xde
+                        , 0xdd
+                        , 0xab
+                        , 0x2d
+                        , 0xb2
+                        , 0x7d
+                        , 0xf1
+                        , 0x3a
+                        , 0x97
+                        , 0x09
+                        , 0xe9
+                        , 0x58
+                        , 0xf0
+                        , 0x8e
+                        , 0x19
+                        , 0xa6
+                        , 0x46
+                        , 0x50
+                        , 0xcb
+                        , 0xb8
+                        , 0x83
+                        , 0xf7
+                        , 0x74
+                        , 0x91
+                        , 0xc5
+                        , 0x94
+                        , 0xc0
+                        , 0x4d
+                        , 0x04
+                        , 0xc5
+                        , 0x40
+                        , 0x5f
+                        , 0x7d
+                        , 0x53
+                        , 0x45
+                        , 0xcd
+                        , 0x03
+                        , 0xf2
+                        , 0x4f
+                        , 0x82
+                        , 0xad
+                        , 0x0d
+                        ]
+            encode (Primitive Ed25519Sig raw64)
+                `shouldBe` "0BA-meOtrZqh25qrCF30C_TuNDz5Et2F3t2rLbJ98TqXCelY8I4ZpkZQy7iD93SRxZTATQTFQF99U0XNA_JPgq0N"
